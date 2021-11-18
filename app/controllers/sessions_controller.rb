40d4212eeec0adbd8251d181ff7492233e98bdb9
Yewei <yeweichen1@gmail.com>
2021-11-18 10:43:20 -0600

    Login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,15 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(user_name: params[:user_name])
-        if user
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: {error: 'invalid credentials'}, status: :unauthorized
+        end
     end
 
     def destroy
-
+        session.delete(:user_id)
     end
 end
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import LoggedIn from './LoggedIn'
 import Login from './Login'
+import {useState} from 'react'
 
 function Header() {
+
+
+
+
     return (
         <div class="Header">
-            <Login />
+            <Login /> : <LoggedIn />
             <h3>Recommended Books</h3>
         </div>
     )
