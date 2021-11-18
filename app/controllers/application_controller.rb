e1cc0722ef19ab834a8894ed99284210b640f376
Yewei <yeweichen1@gmail.com>
2021-11-17 21:46:38 -0600

    fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   def current_user
-    User.find_by{id: session[:user_id]}
+    User.find_by(id: session[:user_id])
   end
 end
